2464655722510b4990c7aba2670f1640efd0cc28
okadata <taiki.okada@justsystems.com>
2019-09-24 15:31:03 +0900

    spec5: IDの出力を実装

--- DicApp/DicApp/DicApp.cpp
+++ DicApp/DicApp/DicApp.cpp
@@ -7,11 +7,28 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <sstream>
 
 typedef std::string String;
 
+class word_info {
+public:
+    word_info( int aID, const std::string& aWord )
+    :mID(aID), mWord(aWord)
+    {
+    }
+    
+    const String get_string_expression() const
+    {
+        std::stringstream xStream;
+        xStream << mID << ": " << mWord;
+        return xStream.str();
+    }
+    
+    std::string mWord; // 表記
+    int mID; // ID（1始まりの数値）
+};
 
-typedef std::string     word_info;
 typedef std::vector<word_info> Words;
 const size_t kReadBufSize = 256;
 
@@ -24,7 +41,7 @@ void output_line(const String& aString)
 // 単語を１つコンソールに出力する
 void output_word(const word_info& aWord)
 {
-    output_line(aWord);
+    output_line(aWord.get_string_expression());
 }
 
 
@@ -39,9 +56,11 @@ Words load_dictionary_data()
     }
 
     char xWord[kReadBufSize] = { 0 };
+    int xID = 1;
     while (xfs.getline(xWord, kReadBufSize - 1)) {
-
-        xWords.push_back(xWord);
+        word_info xWordInfo( xID, xWord );
+        xWords.push_back(xWordInfo);
+        ++xID;
     }
 
     return xWords;
@@ -52,8 +71,7 @@ int main()
 {
     Words xWords = load_dictionary_data();
     for (auto xIter = xWords.begin(); xIter != xWords.end(); ++xIter) {
-
-        std::cout << xIter->c_str() << std::endl;
+        output_word( *xIter );
     }
  
     return 0;
