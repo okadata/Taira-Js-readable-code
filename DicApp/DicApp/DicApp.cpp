720a321333fbd11f38bf45299a70c75eb081353f
taira <fujito.taira@justsystems.com>
2019-09-24 13:31:33 +0900

    Spec3: 外部ファイルの辞書データを読み込みコンソールに出力する
     * 外部ファイルは「dictionary_data.txt」。実行ファイルと同じ場所に置かれる前提

--- DicApp/DicApp/DicApp.cpp
+++ DicApp/DicApp/DicApp.cpp
@@ -3,12 +3,57 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+typedef std::string String;
+
+
+typedef std::string     word_info;
+typedef std::vector<word_info> Words;
+const size_t kReadBufSize = 256;
+
+// 指定した文字列を1行コンソールに出力する
+void output_line(const String& aString)
+{
+    std::cout << aString.c_str() << std::endl;
+}
+
+// 単語を１つコンソールに出力する
+void output_word(const word_info& aWord)
+{
+    output_line(aWord);
+}
+
+
+Words load_dictionary_data()
+{
+    Words xWords;
+     
+    std::ifstream xfs(".\\dictionary_data.txt");
+    if (xfs.fail()) {
+        return xWords;
+    }
+
+    char xWord[kReadBufSize] = { 0 };
+    while (xfs.getline(xWord, kReadBufSize - 1)) {
+
+        xWords.push_back(xWord);
+    }
+
+    return xWords;
+}
 
 
 int main()
 {
-    std::cout << "上手" << std::endl;
-  
+    Words xWords = load_dictionary_data();
+    for (auto xIter = xWords.begin(); xIter != xWords.end(); ++xIter) {
+
+        std::cout << xIter->c_str() << std::endl;
+    }
+ 
     return 0;
 }
 
